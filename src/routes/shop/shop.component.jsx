8dc4f31a5ae2e744b095566ddc5d3f6a0afd15f4
candicefdev <102930486+candicefdev@users.noreply.github.com>
2022-05-28 13:36:59 -0400

    before styled-components was implemented

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,15 @@
-import { useContext } from 'react';
-
-import { ProductsContext } from '../../contexts/products.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.components';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  // give me the products from useContext, passing in the product context
-  const { products } = useContext(ProductsContext);
-
   return (
-    <div className='products-container'>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 };
 
